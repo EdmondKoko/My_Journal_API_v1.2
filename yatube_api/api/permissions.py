e0b5a55a413a78f63fcadc6b0829accabfe5807b
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-17 19:07:11 +0300

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,12 +2,16 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    """Пользователь может просматривать любые записи, но может изменять или удалять только свои записи."""
+    
     def has_permission(self, request, view):
+        """Проверка наличия разрешения на выполнение действия."""
         if (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated):
             return True
 
     def has_object_permission(self, request, view, obj):
+        """Проверка наличия разрешения на выполнение действия над объектом."""
         if obj.author:
             author = obj.author
         else:
